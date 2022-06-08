c2d60a44767089706db5277270dc0239df2d3ac8
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-06-08 12:06:11 +0900

    event의 offsetX, Y 값 가져오기

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 
 function onMouseMove(event) {
-    console.log(event);
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x, y);
 }
 
 if (canvas) {

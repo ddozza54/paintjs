d7c40e9d40aee35b65ae7668c1ba8abff141227f
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-06-12 03:04:20 +0900

    path와 line을 이용해 실제 선 만들기

--- app.js
+++ app.js
@@ -16,20 +16,26 @@ function startPainting() {
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        //클릭을 하고 움직이면(페인팅 시작하면) 이 부분은 작동하지 않음. 
+    }
+    else {
+        ctx.lineTo();
+        ctx.stroke();
+    }
 }
 //마우스를 클릭한 상태 
 function onMouseDown(event) {
     painting = true;
 }
-//마우스를 뗌. 
-function onMouseUp(event) {
-    stopPainting();
-}
+
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove)
-    canvas.addEventListener("mousedown", onMouseDown)
-    canvas.addEventListener("mouseup", onMouseUp) 
+    canvas.addEventListener("mousedown", startPainting)
+    canvas.addEventListener("mouseup", stopPainting)
     //stopPainting을 안넣은 이유 : 마우스를 떼도 라인이 필요. 
     canvas.addEventListener("mouseleave", stopPainting) //캔버스를 벗어남
 }

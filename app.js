020234b475d51b0afddbdc02e4c9e571fef8c646
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-06-17 14:36:46 +0900

    paint와 filling 겹치지 않게

--- app.js
+++ app.js
@@ -64,7 +64,9 @@ function handleModeClick() {
 }
 
 function handleCanvasClick() {
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    if (filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
 }
 
 if (canvas) {
